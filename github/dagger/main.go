c8553fee8d0a93b0bb4a08870803d24e3c78a05d
mihai <mih.gogu@gmail.com>
2024-04-03 17:26:44 +0300

    add github module for test

--- github/dagger/main.go
+++ github/dagger/main.go
@@ -1,59 +1,72 @@
+// This module handles the GitHub API interactions.
+//
+// The CreatePullRequest function creates a new pull request in the provided repository.
 package main
 
-import (
-	"context"
-	"fmt"
-	"github.com/google/go-github/v60/github"
-)
+import "github.com/google/go-github/github"
 
 type GitHub struct {
 	// +private
 	Client *github.Client
 }
 
-func New(
-	// +optional
-	token *Secret,
-) (*GitHub, error) {
+func test() {
 	gh := &GitHub{}
-
-	// Init GitHub client
-	ctx := context.Background()
-	t, err := token.Plaintext(ctx)
-	if err != nil {
-		return gh, fmt.Errorf("failed to get token: %v", err)
-	}
-	gh.Client = github.NewClient(nil).WithAuthToken(t)
-
-	return gh, nil
+	gh.Client = github.NewClient(nil)
 }
 
-func (m *GitHub) CreatePullRequest(
-	// +default="New PR"
-	title string,
-	// +optional
-	// +default="main"
-	baseBranch string,
-) {
-	ctx := context.Background()
+// New creates a new GitHub module with the provided token
+//
+// Example usage: dagger call --token=env:TOKEN create-pull-request
+//func New(
+//	token *Secret,
+//) (*GitHub, error) {
+//	gh := &GitHub{}
+//
+//	// Init GitHub client
+//	ctx := context.Background()
+//	t, err := token.Plaintext(ctx)
+//	if err != nil {
+//		return gh, fmt.Errorf("failed to get token: %v", err)
+//	}
+//	gh.Client = github.NewClient(nil).WithAuthToken(t)
+//
+//	return gh, nil
+//}
 
-	// Get the reference of the branch you want to create from
-	ref, _, err := m.Client.Git.GetRef(ctx, "owner", "repo", "refs/heads/main")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// Create a new reference for the new branch
-	newRef := &github.Reference{
-		Ref: github.String("refs/heads/new-branch"),
-		Object: &github.GitObject{
-			SHA: ref.Object.SHA,
-		},
-	}
-	_, _, err = m.Client.Git.CreateRef(ctx, "owner", "repo", newRef)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-}
+//func (m *GitHub) CreatePullRequest(
+//	// +default="New PR"
+//	title,
+//	// +optional
+//	// +default="main"
+//	baseBranch,
+//	// +optional
+//	// +default="upgrade-istio"
+//	newBranch,
+//	// +required
+//	owner,
+//	// +required
+//	repo string,
+//) error {
+//	ctx := context.Background()
+//
+//	// Get the reference of the branch you want to create from
+//	ref, _, err := m.Client.Git.GetRef(ctx, owner, repo, "refs/heads/"+baseBranch)
+//	if err != nil {
+//		return fmt.Errorf("failed to get ref: %v", err)
+//	}
+//
+//	// Create a new reference for the new branch
+//	newRef := &github.Reference{
+//		Ref: github.String("refs/heads/" + newBranch),
+//		Object: &github.GitObject{
+//			SHA: ref.Object.SHA,
+//		},
+//	}
+//	_, _, err = m.Client.Git.CreateRef(ctx, owner, repo, newRef)
+//	if err != nil {
+//		return fmt.Errorf("failed to create ref: %v", err)
+//	}
+//
+//	return nil
+//}
