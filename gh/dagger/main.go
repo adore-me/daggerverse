18355ef2a8b6319c166278ffa9aa92e774c91db1
mihai <mih.gogu@gmail.com>
2024-04-17 18:23:20 +0300

    Update github module - test

--- gh/dagger/main.go
+++ gh/dagger/main.go
@@ -46,8 +46,8 @@ func (m *Gh) RunGit(
 	// +optional
 	// +default="2.43.0"
 	version string,
-) (*Directory, error) {
-	c := dag.Container().
+) (string, error) {
+	c, err := dag.Container().
 		From("alpine/git:"+version).
 		WithDirectory("/workspace", repoPath, ContainerWithDirectoryOpts{}).
 		WithSecretVariable("GITHUB_TOKEN", m.Token).
@@ -55,9 +55,12 @@ func (m *Gh) RunGit(
 		WithExec(
 			[]string{"sh", "-c", strings.Join([]string{"git", cmd}, " ")},
 			ContainerWithExecOpts{SkipEntrypoint: true},
-		)
+		).Sync(ctx)
+	if err != nil {
+		return "", fmt.Errorf("failed to run git command: %w", err)
+	}
 
-	return c.Directory("/workspace"), nil
+	return c.Stdout(ctx)
 }
 
 // RunGh runs a command using the git CLI.
