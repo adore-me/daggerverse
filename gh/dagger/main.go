1898d7fd5bb9f880dae7e1bc8823744f2dfde97d
mihai <mih.gogu@gmail.com>
2024-04-17 17:19:25 +0300

    Update github module - added sync

--- gh/dagger/main.go
+++ gh/dagger/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"strings"
 )
 
@@ -46,7 +47,7 @@ func (m *Gh) RunGit(
 	// +default="2.43.0"
 	version string,
 ) (string, error) {
-	return dag.Container().
+	c, err := dag.Container().
 		From("alpine/git:"+version).
 		WithDirectory("/workspace", repoPath, ContainerWithDirectoryOpts{}).
 		WithSecretVariable("GITHUB_TOKEN", m.Token).
@@ -54,7 +55,12 @@ func (m *Gh) RunGit(
 		WithExec(
 			[]string{"sh", "-c", strings.Join([]string{"git", cmd}, " ")},
 			ContainerWithExecOpts{SkipEntrypoint: true},
-		).Stdout(ctx)
+		).Sync(ctx)
+	if err != nil {
+		return "", fmt.Errorf("failed to run git command: %w", err)
+	}
+
+	return c.Stdout(ctx)
 }
 
 // RunGh runs a command using the git CLI.
@@ -73,7 +79,7 @@ func (m *Gh) RunGh(
 	// +default="2.47.0"
 	version string,
 ) (string, error) {
-	return dag.Container().
+	c, err := dag.Container().
 		From("maniator/gh:v"+version).
 		WithDirectory("/workspace", repoPath, ContainerWithDirectoryOpts{}).
 		WithSecretVariable("GITHUB_TOKEN", m.Token).
@@ -81,5 +87,10 @@ func (m *Gh) RunGh(
 		WithExec(
 			[]string{"sh", "-c", strings.Join([]string{"gh", cmd}, " ")},
 			ContainerWithExecOpts{SkipEntrypoint: true},
-		).Stdout(ctx)
+		).Sync(ctx)
+	if err != nil {
+		return "", fmt.Errorf("failed to run git command: %w", err)
+	}
+
+	return c.Stdout(ctx)
 }
